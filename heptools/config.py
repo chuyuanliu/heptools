ce4fc8a0340f11459631c15f2f86eabcf90eb68f
ChuyuanLiu <chuyuanliu96@gmail.com>
2023-08-01 23:06:03 -0400

    fix config inheritance

--- heptools/config.py
+++ heptools/config.py
@@ -13,17 +13,30 @@ class ConfigError(Exception):
 Undefined = object()
 
 class Config:
-    __default__ = {}
     __reserve__ = ['update', 'reset', 'report']
+    __default__ = {}
     __unified__ = []
+    __updated__ = {}
 
     @classmethod
-    def __print_parameter__(cls, __par, __type = Any, __value = Undefined):
-        __match = __value is Undefined or isinstance_(__value, __type)
-        args = {} if __match else {'style': 'red'}
-        __type  = Text(' : ') + Text(f'{type_name(__type)}', style = 'green') if __type is not Any else Text('')
-        __value = Text(' = ') + Text(f'{__value}', style = 'blue') if __value is not Undefined else Text('')
-        return Text(f'{__par}', **args) + __type + __value
+    def __overridden__(cls):
+        overridden = []
+        class base(*cls.__bases__):
+            ...
+        for par in cls.__parameters__:
+            if hasattr(base, par) and getattr(base, par) is not getattr(cls, par):
+                overridden.append(par)
+        return overridden
+
+    @classmethod
+    def __print_parameter__(cls, __par, __type = Any, __value = Undefined, __source = None):
+        args = {} if __value is Undefined or isinstance_(__value, __type) else {'style': 'red'}
+        if __source is None:
+            __source = cls.__name__
+        __source = Text(' (') + Text(f'{__source}', style = 'yellow') + Text(')') if __value is not Undefined else Text('')
+        __type   = Text(' : ') + Text(f'{type_name(__type)}', style = 'green') if __type is not Any else Text('')
+        __value  = Text(' = ') + Text(f'{__value}', style = 'blue') if __value is not Undefined else Text('')
+        return Text(f'{__par}', **args) + __type + __value + __source
 
     @classmethod
     @property
@@ -33,7 +46,7 @@ class Config:
         return {k: hints.get(k, Any) for k in keys if not ((k.startswith('__') and k.endswith('__')))}
 
     @classmethod
-    def update(cls, *configs):
+    def update(cls, *configs: Config):
         for config in configs:
             if not issubclass(config, cls):
                 raise ConfigError(f'cannot update {cls.__name__} with {config.__name__}')
@@ -42,11 +55,12 @@ class Config:
             if diff:
                 raise ConfigError(f'cannot update {cls.__name__} with {config.__name__} without {diff}')
             cls.__unified__.append(config)
-            for par in cls.__parameters__:
+            for par in set(cls.__parameters__) & set(config.__overridden__()):
                 v_old = getattr(cls, par)
                 v_new = getattr(config, par)
                 if v_old is not v_new:
                     setattr(cls, par, v_new)
+                    cls.__updated__[par] = config.__name__
                     if par not in cls.__default__:
                         cls.__default__[par] = v_old
 
@@ -56,6 +70,7 @@ class Config:
             setattr(cls, k, v)
         cls.__default__ = {}
         cls.__unified__ = []
+        cls.__updated__ = {}
 
     @classmethod
     def report(cls):
@@ -64,7 +79,8 @@ class Config:
         pars = cls.__parameters__
         for __par in sorted(list(pars)):
             __value = getattr(cls, __par)
-            __str = cls.__print_parameter__(__par, pars[__par], __value)
+            __source = cls.__updated__.get(__par, None)
+            __str = cls.__print_parameter__(__par, pars[__par], __value, __source)
             if __value is Undefined:
                 undefined_pars.append(__str)
             else:
