b58809d033962a55d1767fc50abc9fa4cce7235a
ChuyuanLiu <chuyuanliu96@gmail.com>
2023-08-10 17:59:35 -0400

    improve compression efficiency

--- heptools/io/__init__.py
+++ heptools/io/__init__.py
@@ -1,9 +1,18 @@
-import gzip
+import importlib
 import pickle
+from typing import Literal
 
+Compression = Literal['gzip', 'bz2', 'lzma']
 
-def save(fname: str, obj):
-    pickle.dump(obj, gzip.open(fname, 'wb'))
+def open_zip(algo: Compression, file: str, mode: str, **kwargs):
+    mod = importlib.import_module(algo)
+    default = {}
+    if algo in ['gzip', 'bz2']:
+        default['compresslevel'] = 4
+    return mod.open(file, mode, **(default | kwargs))
 
-def load(fname: str):
-    return pickle.load(gzip.open(fname, 'rb'))
+def save(fname: str, obj, algo: Compression = 'gzip', **kwargs):
+    pickle.dump(obj, open_zip(algo, fname, 'wb', **kwargs))
+
+def load(fname: str, algo: Compression = 'gzip', **kwargs):
+    return pickle.load(open_zip(algo, fname, 'rb', **kwargs))
