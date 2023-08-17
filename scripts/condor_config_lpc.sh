e735411aa11100ee7009e7c45ac58e1c01af938e
ChuyuanLiu <chuyuanliu96@gmail.com>
2023-08-16 20:12:42 -0400

    fix typo

--- scripts/condor_config_lpc.sh
+++ scripts/condor_config_lpc.sh
@@ -1,7 +1,7 @@
 export CONDOR_CONFIG="${HOME}/.condor/condor_config"
 grep -v "^include" "/etc/condor/config.d/01_cmslpc_interactive" > "${CONDOR_CONFIG}"
 get_host(){
-    if [ -z "${1}"]
+    if [ -z "${1}" ]
     then
         local host="$(grep "^SCHEDD_HOST" "/storage/local/data1/condor/config.d/${USER}.config")"
     else
