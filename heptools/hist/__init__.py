7f59e364c56313631b87c1e05a6d7fb0bf221f11
ChuyuanLiu <chuyuanliu96@gmail.com>
2023-08-18 23:23:28 -0400

    update environment.yml

--- heptools/hist/__init__.py
+++ heptools/hist/__init__.py
@@ -1,10 +1,8 @@
 from .fill import Fill, FillError
 from .hist import HistError
 from .hist import Set as Hists
-from .plot import Plot
 from .subsets import DiFourvector, Fourvector, Systematic
 
-__all__ = ['Hists',
-           'Fill', 'Plot',
+__all__ = ['Hists', 'Fill',
            'Fourvector', 'DiFourvector', 'Systematic',
            'FillError', 'HistError']
