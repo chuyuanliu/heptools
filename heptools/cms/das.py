ab63e0ef7de56495020063b18b18c2ef5fabdeee
ChuyuanLiu <chuyuanliu96@gmail.com>
2023-11-02 12:07:42 -0400

    update das query

--- heptools/cms/das.py
+++ heptools/cms/das.py
@@ -2,41 +2,35 @@
 |               |                                   |
 | -             | -                                 |
 | DAS           | https://cmsweb.cern.ch/das/       |
-| client        | https://github.com/dmwm/DBSClient |
+| Rucio         | https://rucio.cern.ch/            |
+| DBS3-Client   | https://github.com/dmwm/DBSClient |
 '''
-# TODO migrate to DBSClient
-# url https://cmsweb.cern.ch/dbs/prod/global/DBSReader/
-import json
-from multiprocessing import Pool
-from subprocess import check_output
+from dbs.apis.dbsClient import DbsApi
+from rucio.client import Client as RucioClient
 
 __all__ = ['DASError', 'DAS']
 
 class DASError(Exception):
     __module__ = Exception.__module__
 
+
 class DAS:
-    @staticmethod
-    def client(*queries: str, to_json = True, **kwargs: str):
-        if not queries or not kwargs:
-            raise DASError(f'invalid query "{queries} {kwargs}"')
-        result = check_output(f'dasgoclient{" -json" if to_json else ""} -query="{",".join(queries)} {" ".join(f"{k}={v}" for k, v in kwargs.items())}"', shell=True)
-        if to_json:
-            result = json.loads(result)
-        else:
-            result = result.decode('utf-8')
-        return result
+    dbs3  = DbsApi('https://cmsweb.cern.ch/dbs/prod/global/DBSReader/')
+    rucio = RucioClient()
 
-    @staticmethod
-    def query_file_mp(file):
-        return {'path': file['file'][0]['name'],
-                'nevents': file['file'][0]['nevents'],
-                'site': DAS.client('site', file = file['file'][0]['name'], to_json = False).split('\n')}
-
-    @staticmethod
-    def query_dataset(query: str):
-        files = DAS.client('file', dataset = query)
+    @classmethod
+    def query(cls, dataset: str):
+        # DBS3 query
+        files = cls.dbs3.listFiles(dataset = dataset, detail = True)
         if not files:
-            raise DASError(f'no files found for dataset "{query}"')
-        with Pool(processes = len(files)) as pool:
-            return {'files': pool.map(DAS.query_file_mp, files)}
+            raise DASError(f'no files found for dataset "{dataset}"')
+        files = {file['logical_file_name']: {
+            'path'      : file['logical_file_name'],
+            'nevents'   : file['event_count']
+        } for file in files}
+        # Rucio query
+        for replicas in cls.rucio.list_replicas(
+            dids = [{'scope': 'cms', 'name': file} for file in files],
+            schemes = ['root']):
+            files[replicas['name']]['site'] = [site for site, status in replicas['states'].items() if status == 'AVAILABLE']
+        return {'files': list(files.values())}
