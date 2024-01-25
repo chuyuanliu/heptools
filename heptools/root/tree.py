f9a89b6ada1d93c8311a74e7bb29c07a047fcce3
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-01-25 12:11:25 -0500

    fix bugs

--- heptools/root/tree.py
+++ heptools/root/tree.py
@@ -1,6 +1,8 @@
 # TODO
 from __future__ import annotations
 
+import bisect
+from collections import defaultdict
 from functools import partial
 from threading import Thread
 from typing import Iterable
@@ -87,7 +89,7 @@ class Chunk(metaclass=_ChunkMeta):
         entry_stop: int = ...,
         fetch: bool = False,
     ):
-        if branches is not ...:
+        if isinstance(branches, Iterable):
             branches = {*branches}
 
         self.name = name
@@ -122,35 +124,43 @@ class Chunk(metaclass=_ChunkMeta):
                         self._entry_stop = num_entries
                 elif self._num_entries != num_entries:
                     raise ValueError(
-                        f'Inconsistent number of entries: [self]{self._num_entries} != [file]{num_entries}')
+                        f'The number of entries in the file {num_entries} does not match the stored one {self._num_entries}')
 
                 uuid = file.file.uuid
                 if self._uuid is ...:
                     self._uuid = uuid
                 elif self._uuid != uuid:
                     raise ValueError(
-                        f'Inconsistent UUID: [self]{self._uuid} != [file]{uuid}')
+                        f'The UUID of the file {uuid} does not match the stored one {self._uuid}')
 
     def __hash__(self):
-        return hash((self.uuid, self.name, self.num_entries))
+        return hash((self.uuid, self.name))
 
     def __eq__(self, other):
         if isinstance(other, Chunk):
-            return (self.uuid, self.name, self.num_entries) == (other.uuid, other.name, other.num_entries)
+            return (self.uuid, self.name) == (other.uuid, other.name)
         return NotImplemented
 
     def __len__(self):
         return self.entry_stop - self.entry_start
 
     def __repr__(self):
-        return f'TTree:{self.path}({self.uuid}):{self.name}[{self.entry_start},{self.entry_stop}) \u2286 [0,{self.num_entries})'
+        tree = f'TTree:{self.path}'
+        if self._uuid is not ...:
+            tree += f'({self._uuid})'
+        tree += f':{self.name}'
+        if self._entry_stop is not ...:
+            tree += f'[{self.entry_start},{self._entry_stop})'
+        if self._num_entries is not ...:
+            tree += f' \u2286 [0,{self._num_entries})'
+        return tree
 
     def __json__(self):
         return {
             'path': str(self.path),
             'uuid': str(self.uuid),
             'name': self.name,
-            'branches': list(self.branches),
+            'branches': list(self.branches) if self.branches else self.branches,
             'num_entries': self.num_entries,
             'entry_start': self.entry_start,
             'entry_stop': self.entry_stop,
@@ -193,24 +203,26 @@ class Chunk(metaclass=_ChunkMeta):
             invalid |= stop > self._entry_stop
         if invalid:
             raise ValueError(
-                f'Invalid slice: [{start},{stop}) \u2284 [{self.entry_start},{self._entry_stop})')
+                f'The slice is not a subset of the chunk [{start},{stop}) \u2284 [{self.entry_start},{self._entry_stop})')
         chunk = self.copy()
         chunk.entry_start = start
         chunk._entry_stop = stop
         return chunk
 
-    def id(self):
+    def source(self):
         """
         Returns
         -------
         Chunk
-            A :meth:`copy` of ``self`` removing :data:`branches`, :data:`entry_start` and :data:`entry_stop`.
+            A :meth:`copy` of ``self`` keeping only :data:`path`, :data:`uuid` and :data:`name`.
         """
         return Chunk(
             source=(self.path, self.uuid),
             name=self.name,
-            num_entries=self.num_entries,
-            branches=())
+            num_entries=None,
+            branches=None,
+            entry_start=None,
+            entry_stop=None)
 
     @classmethod
     def from_path(cls, *paths: str):
@@ -271,14 +283,97 @@ class Chunk(metaclass=_ChunkMeta):
         if group:
             yield group
 
+    @classmethod
+    def from_json(cls, data: dict):
+        """
+        Create :class:`Chunk` from JSON data.
+
+        Parameters
+        ----------
+        data : dict
+            JSON data.
+
+        Returns
+        -------
+        Chunk
+            Chunk from JSON data.
+        """
+        return cls(
+            source=(data['path'], UUID(data['uuid'])),
+            name=data['name'],
+            branches=data['branches'],
+            num_entries=data['num_entries'],
+            entry_start=data['entry_start'],
+            entry_stop=data['entry_stop'])
+
+    @classmethod
+    def from_coffea_processor(cls, events):
+        """
+        Create :class:`Chunk` from the input of :meth:`coffea.processor.ProcessorABC.process`.
+        """
+        metadata = events.metadata
+        return cls(
+            source=(metadata['filename'], UUID(metadata['fileuuid'])),
+            name=metadata['treename'],
+            entry_start=metadata['entrystart'],
+            entry_stop=metadata['entrystop'])
+
+
+class _FriendItem:  # TODO
+    def __init__(self, start: int, end: int, chunk: Chunk):
+        self.start = start
+        self.end = end
+        self.chunk = chunk
+
+    def __lt__(self, other):
+        if isinstance(other, _FriendItem):
+            return self.end <= other.start
+        return NotImplemented
+
+    def __repr__(self):
+        return f'[{self.start},{self.end})'
+
+    def __json__(self):
+        return {
+            'start': self.start,
+            'end': self.end,
+            'chunk': self.chunk,
+        }
+
+    @classmethod
+    def from_json(cls, data: dict):
+        return cls(data['start'], data['end'], data['chunk'])
+
 
 class Friend:  # TODO
     """
     A tool to create and manage addtional :class:`TBranch` stored in separate ROOT files. (also known as friend :class:`TTree`)
     """
 
-    def __init__(self):
-        ...  # TODO
+    def __init__(self):  # TODO
+        self._branches: set[str] = None
+        self._data: defaultdict[Chunk, list[_FriendItem]] = defaultdict(list)
+        self._to_dump: list[_FriendItem] = []
+
+    def _insert(self, tree: Chunk, item: _FriendItem):
+        series = self._data[tree]
+        idx = bisect.bisect_left(series, item)
+        if idx >= len(series):
+            series.append(item)
+        else:
+            exist = series[idx]
+            if item.end <= exist.start:
+                series.insert(idx, item)
+            else:
+                raise ValueError(
+                    f'The new chunk {item} overlaps with the existing one {exist} when inserting into {tree}')
+
+    def __add__(self, other):
+        if isinstance(other, Friend):
+            if self._branches != other._branches:
+                raise ValueError(
+                    'Cannot merge friend trees with different branches')
+        return NotImplemented
 
     def add(
         self,
