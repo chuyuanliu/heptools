d8d4cb866632deeed1848fa90ac9acbd8c062bc0
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-01-30 19:37:16 -0500

    add wrapper

--- heptools/dask/delayed.py
+++ heptools/dask/delayed.py
@@ -1,21 +1,17 @@
 from functools import wraps
 
-
-class _Delayed:
-    def __init__(self, func):
-        self._func = func
-        self._delayed = None
-        self.__doc__ = func.__doc__
-
-    def __call__(self, *args, dask: bool = False, **kwargs):
-        if dask:
-            if self._delayed is None:
-                from dask import delayed
-                self._delayed = delayed(self._func)
-            return self._delayed(*args, **kwargs, dask=dask)
-        else:
-            return self._func(*args, **kwargs, dask=dask)
+from ..utils.wrapper import OptionalDecorator
 
 
-def delayed(func):
-    return wraps(func)(_Delayed(func))
+class _Delayed(OptionalDecorator):
+    @property
+    def _switch(self):
+        return 'dask'
+
+    def _decorate(self, __func):
+        from dask import delayed
+        return delayed(__func)
+
+
+def delayed(__func):
+    return wraps(__func)(_Delayed(__func))
---
+++ heptools/utils/wrapper.py
@@ -0,0 +1,46 @@
+
+
+import inspect
+from abc import ABC, abstractmethod
+from functools import partial
+from typing import Callable
+
+
+class OptionalDecorator(ABC):
+    @property
+    @abstractmethod
+    def _switch(cls) -> str:
+        ...
+
+    @abstractmethod
+    def _decorate(cls, __func: Callable, **kwargs) -> Callable:
+        ...
+
+    def __new__(cls, __func=None, **kwargs):
+        if __func is None:
+            return partial(cls, **kwargs)
+        return super().__new__(cls)
+
+    def __init__(self, __func, **kwargs):
+        signature = inspect.signature(__func)
+        if self._switch not in signature.parameters:
+            raise ValueError(
+                f'Function "{__func.__name__}{signature}" must have a parameter "{self._switch}: bool"')
+        self._func = __func
+        self._decorated = None
+        self._default = signature.parameters[self._switch].default
+        if self._default is inspect.Signature.empty:
+            self._default = False
+        self._kwargs = kwargs
+
+    def __get__(self, instance, owner):
+        return partial(self.__call__, instance)
+
+    def __call__(self, *args, **kwargs):
+        kwargs.setdefault(self._switch, self._default)
+        if kwargs[self._switch]:
+            if self._decorated is None:
+                self._decorated = self._decorate(self._func, **self._kwargs)
+            return self._decorated(*args, **kwargs)
+        else:
+            return self._func(*args, **kwargs)
