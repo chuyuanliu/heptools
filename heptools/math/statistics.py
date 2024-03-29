4160a2135d52a4e3a62d85a15530a85a4b3f178c
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-03-29 00:48:11 -0400

    add unbiased variance

--- heptools/math/statistics.py
+++ heptools/math/statistics.py
@@ -6,7 +6,7 @@ from typing import Generic, TypeVar
 
 _DataT = TypeVar("_DataT")
 _VarianceSelf = TypeVar("_VarianceSelf", bound="Variance")
-_VarianceData = namedtuple("_VarianceBase", ["sumw", "m1", "M2"])
+_VarianceData = namedtuple("_VarianceBase", ["sumw", "sumw2", "m1", "M2"])
 
 
 class Variance(ABC, Generic[_DataT]):
@@ -20,11 +20,11 @@ class Variance(ABC, Generic[_DataT]):
     @abstractmethod
     def calculate(
         cls, data: _DataT, weight: _DataT = None
-    ) -> tuple[_DataT, _DataT, _DataT]: ...
+    ) -> tuple[_DataT, _DataT, _DataT, _DataT]: ...
 
     def __init__(self, data: _DataT = None, weight: _DataT = None):
         if data is None:
-            self._raw = _VarianceData(None, None, None)
+            self._raw = None
         else:
             self._raw = _VarianceData(*self.calculate(data, weight))
 
@@ -36,16 +36,22 @@ class Variance(ABC, Generic[_DataT]):
     def variance(self) -> _DataT:
         return self._raw.M2 / self._raw.sumw
 
+    @property
+    def variance_unbiased(self) -> _DataT:
+        """https://en.wikipedia.org/wiki/Weighted_arithmetic_mean#Reliability_weights"""
+        return self.variance / (1 - self._raw.sumw2 / (self._raw.sumw * self._raw.sumw))
+
     def __add__(self: _VarianceSelf, other: _VarianceSelf) -> _VarianceSelf:
         if not isinstance(other, type(self)):
             return NotImplemented
         new = type(self)()
-        if self._raw.sumw is None:
+        if self._raw is None:
             new._raw = other._raw
-        elif other._raw.sumw is None:
+        elif other._raw is None:
             new._raw = self._raw
         else:
             sumw = self._raw.sumw + other._raw.sumw
+            sumw2 = self._raw.sumw2 + other._raw.sumw2
             delta = other._raw.m1 - self._raw.m1
             m1 = self._raw.m1 + delta * other._raw.sumw / sumw
             M2 = (
@@ -53,5 +59,5 @@ class Variance(ABC, Generic[_DataT]):
                 + other._raw.M2
                 + delta**2 * self._raw.sumw * other._raw.sumw / sumw
             )
-            new._raw = _VarianceData(sumw, m1, M2)
+            new._raw = _VarianceData(sumw, sumw2, m1, M2)
         return new
