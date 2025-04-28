7070e4c491ee9226a495b7741bda98ff0186f401
ChuyuanLiu <chuyuanliu96@gmail.com>
2025-04-28 19:47:42 -0400

    update actions

--- docs/build.sh
+++
@@ -1,8 +0,0 @@
-# compile scss to css
-cd _static/css
-for input in *.scss; do
-    if [[ $input != _* ]]; then
-        output=${input%.scss}.css
-        sass --style compressed --no-source-map $input $output
-    fi
-done
